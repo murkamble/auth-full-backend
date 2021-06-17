687f6c506899b75c5da5222afdc1829868f9ae98
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-06-17 15:06:11 +0530

    First Commit

---
+++ src/models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name!"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email!"],
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password!"]
+    },
+    role: {
+        type: Number,
+        default: 0 // 0 = user, 1 = admin
+    },
+    avatar: {
+        type: String,
+        default: "https://res.cloudinary.com/mayurkamble/image/upload/v1623832874/146-1468479_my-profile-icon-blank-profile-picture-circle-hd_pbd3h3.png"
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Users", userSchema)
